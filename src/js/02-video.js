d304dace418193bc2e6902406bb8b958c7634f98
Ivan Mutsko <imucko1996@gmail.com>
2023-02-18 17:02:16 +0200

    complite task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,20 @@
 import Player from '@vimeo/player';
 import { throttle } from 'lodash';
 
+// пошук плеєра
 const iframe = document.querySelector('iframe');
+// створення екземпляра
 const player = new Player(iframe);
-
-player.on('play', function () {
-  console.log('played the video!');
-});
-
+// збереження в локалсторедж даних кожну 1 секунду
 player.on(
   'timeupdate',
   throttle(evt => {
     localStorage.setItem('videoplayer-current-time', JSON.stringify(evt));
-    console.log(evt);
   }, 1000)
 );
-
+// отримання даних з локалсторедж
 const savedTime = localStorage.getItem('videoplayer-current-time');
+// розпарсити в об'єкт
 let startPlayTime = JSON.parse(savedTime);
-
+// встановити початковий час
 player.setCurrentTime(startPlayTime.seconds);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const formBtn = document.querySelector('button');
+const LOCALSTORAGE_KEY_FORM = 'feedback-form-state';
+
+// записуємо кожні 500мс введені дані в локалсторедж
+form.addEventListener(
+  'input',
+  throttle(() => {
+    //   шаблон локалстореджа
+    const savedFormString = {
+      email: email.value,
+      message: message.value,
+    };
+    // перевід шаблона в рядок + збереження в локалсторедж
+    localStorage.setItem(
+      LOCALSTORAGE_KEY_FORM,
+      JSON.stringify(savedFormString)
+    );
+  }, 500)
+);
+
+// перевірка на правильність данних при парсі
+try {
+  const savedFormObj = localStorage.getItem(LOCALSTORAGE_KEY_FORM);
+  const parsedSavedFormObj = JSON.parse(savedFormObj);
+  // перевірки на наявність даних в формі і запис зі стореджа
+  if (parsedSavedFormObj.email.value !== '') {
+    email.value = parsedSavedFormObj.email;
+  }
+  if (parsedSavedFormObj.message.value !== '') {
+    message.value = parsedSavedFormObj.message;
+  }
+} catch (error) {
+  // вивід помилок
+  console.log(error.name);
+  console.log(error.message);
+}
+// слухач кнопки
+formBtn.addEventListener('click', event => {
+  event.preventDefault();
+  // очистка стореджа
+  localStorage.removeItem(LOCALSTORAGE_KEY_FORM);
+  // вивід в консоль відправлених даних
+  const submitData = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log('Feedback: ', submitData);
+  // очистка форми
+  form.reset();
+});
