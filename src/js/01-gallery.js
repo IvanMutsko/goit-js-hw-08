48f02ff38963467ab77daa65a2385a9eba1f407e
Ivan Mutsko <imucko1996@gmail.com>
2023-02-18 11:35:33 +0200

    complite task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+// виклик створення розмітки
+makeMarkupGallery(galleryItems);
+// функція створення розмітки
+function makeMarkupGallery(pictures) {
+  const markup = pictures
+    .map(
+      ({
+        description,
+        original,
+        preview,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+  // пушимо в HTML
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+// запуск галереї
+let lightbox = new SimpleLightbox('.gallery a', {
+  // налаштування галереї
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: false,
+  showCounter: false,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on(
+  'timeupdate',
+  throttle(evt => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(evt));
+    console.log(evt);
+  }, 1000)
+);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+let startPlayTime = JSON.parse(savedTime);
+
+player.setCurrentTime(startPlayTime.seconds);
